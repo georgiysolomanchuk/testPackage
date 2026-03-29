1b1b04e1fbb4bd97c144b8e3e36f2105c8f94b1d
georgiysolomanchuk <georgiy_solomanchuk@brown.edu>
2026-03-28 23:00:53 -0400

    my_theme_function

---
+++ R/my_theme.R
@@ -0,0 +1,20 @@
+#' Custom ggplot theme
+#'
+#' @return A ggplot2 theme object.
+#' @export
+#'
+#' @examples
+#' if (requireNamespace("ggplot2", quietly = TRUE)) {
+#'   ggplot2::ggplot(mtcars, ggplot2::aes(wt, mpg)) +
+#'     ggplot2::geom_point() +
+#'     my_theme()
+#' }
+my_theme <- function() {
+  ggplot2::theme_minimal() +
+    ggplot2::theme(
+      plot.title = ggplot2::element_text(size = 14, face = "bold"),
+      axis.title = ggplot2::element_text(size = 12),
+      panel.grid.minor = ggplot2::element_blank(),
+      legend.position = "bottom"
+    )
+}
